42a1e4ee343f2b3e3a304bddf5cebf5691be007f
Russell Brooks <rbrooks@shoprunner.com>
2018-09-04 12:24:19 -0400

    Initial Commit

---
+++ src/main/kotlin/com/journaler/api/controller/NoteController.kt
@@ -0,0 +1,58 @@
+package com.journaler.api.controller
+
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+import org.springframework.beans.factory.annotation.Autowired
+import java.util.*
+import com.journaler.api.data.Note
+import com.journaler.api.service.NoteService
+
+
+@RestController
+@RequestMapping("/notes")
+class NoteController {
+    @Autowired
+    private lateinit var service: NoteService
+    /**
+     * Get notes.
+     */
+    @GetMapping(
+            produces = arrayOf(MediaType.APPLICATION_JSON_VALUE)
+    )
+    fun getNotes() = service.getNotes()
+
+    /**
+     * Insert note.
+     * It consumes JSON, that is: request body Note.
+     */
+    @PutMapping(
+            produces = arrayOf(MediaType.APPLICATION_JSON_VALUE),
+            consumes = arrayOf(MediaType.APPLICATION_JSON_VALUE)
+    )
+    fun insertNote(
+            @RequestBody note: Note
+    ) = service.insertNote(note)
+    /**
+     * Remove note by Id.
+     * We introduced path variable for Id to pass.
+     */
+    @DeleteMapping(
+            value = "/{id}",
+            produces = arrayOf(MediaType.APPLICATION_JSON_VALUE)
+    )
+    fun deleteNote(
+            @PathVariable(name = "id") id: String
+    ): Boolean = service.deleteNote(id)
+    /**
+     * Update item.
+     * It consumes JSON, that is: request body Note.
+     * As result it returns boolean, True == success.
+     */
+    @PostMapping(
+            produces = arrayOf(MediaType.APPLICATION_JSON_VALUE),
+            consumes = arrayOf(MediaType.APPLICATION_JSON_VALUE)
+    )
+    fun updateNote(
+            @RequestBody note: Note
+    ): Boolean = service.updateNote(note)
+}
